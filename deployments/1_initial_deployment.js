f3e54dfb2bbd00e93e2fe78fe761a4610227f208
Luiserebii <luis@serebii.io>
2019-04-22 20:56:28 -0400

    Adding code to find .sol files, pulled some useful boilerplate code from: https://stackoverflow.com/a/25462405

--- deployments/1_initial_deployment.js
+++ deployments/1_initial_deployment.js
@@ -25,3 +25,73 @@ var input = {
 var output = JSON.parse(solc.compile(JSON.stringify(input)))
 console.log(output)
 
+console.log(findFilesInDir(contractsPath, ".sol"))
+
+
+
+function findSolInDir(root) {
+  return findExtInDir(root, ".sol");
+}
+
+function findExtInDir(root, ext) {
+  let condition = (filename) => {
+    return (filename.slice(ext.length * -1) === ext);
+  } 
+  return findFilesInDir(root, condition);
+}
+
+
+//Takes a root filepath, and a condition function
+//Condition function is expected to take a filename and return a bool; i.e., something like:
+// public bool isSol(filename){} (pseudo-code syntax)
+
+function findFilesInDir(root, condition){
+  var results = [];
+
+  if(!fs.existsSync(root)){
+    console.log("No directory found", root);
+    return;
+  }
+
+  var files = fs.readdirSync(root);
+
+  for(f in files){
+
+    var filename = path.join(root, f);
+    var stat = fs.lstatSync(filename);
+
+    if(stat.isDirectory()){
+      results = results.concat(findFilesInDir(filename, condition)); //recurse
+    } else if(condition(filename)) {
+      console.log('-- found: ', filename);
+      results.push(filename);
+    }
+  }
+  return results;
+}
+
+//Generate input object, assuming Solidity as language, from filepath of sources
+//function generateSolcInput(contractsPath) {
+// let input = { language: 'Solidity', sources: {} };
+  //parse through all the paths
+  //For each .sol file found,
+  //  (Grab contents of contract)
+  //  (Grab base filename, which should end in .sol)
+  //  Find contract name (assume we only have 1 potential contract name to parse)
+  
+  //Finally, define input.sources.[name of contract] = {contents: [contentsofcontract]}
+  //return input
+  //for()
+
+//}
+
+//function parseContractName(/*string*/ content) {
+  //for each line...
+  //Look for "contract " as reserved keyword OR "interface "
+  //Read everything from there up to the next whitespace
+
+//}
+
+
+
+
