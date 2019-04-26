1f06a06bc181c04fcf2e1c09ded2818ad2810c70
Luiserebii <luis@serebii.io>
2019-04-26 01:04:09 -0400

    Finished sketching out node code

--- deployments/bash-node-interaction-poc/main-script.js
+++ deployments/bash-node-interaction-poc/main-script.js
@@ -2,4 +2,30 @@ const minimist = require('minimist');
 const args = minimist(process.argv.slice(2));
 
 
+//# node output:
+//# 
+//# Node.js script
+//# ===============
+//# A: [Value of A passed]
+//# B: [Value of B passed]
+//# C: [A + B]
 
+let a = args['a'];
+let b = args['b'];
+
+printAdd(a, b, a + b);
+
+
+function printAdd(a, b, c) {
+  console.log("Node.js script");
+  console.log("===============");
+  console.log("A: " + a);
+  console.log("B: " + b);
+  console.log("C: " + c);
+  console.log("");
+}
+
+
+function add(a, b) { 
+  return a + b;
+}
