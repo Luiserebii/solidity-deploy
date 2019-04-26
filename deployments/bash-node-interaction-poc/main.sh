238eadc357ab66d6153ca01241cfe471ada7b7fe
Luiserebii <luis@serebii.io>
2019-04-25 23:19:21 -0400

    Writing pseudocode for test proof-of-concept

--- deployments/bash-node-interaction-poc/main.sh
+++ deployments/bash-node-interaction-poc/main.sh
@@ -16,18 +16,29 @@ headline () {
 
 main="./main-script.js"
 
-a=1
-b=2
+a=0
+b=1
 
-node $main --stage
+# node output:
+# 
+# Node.js script
+# ===============
+# A: [Value of A passed]
+# B: [Value of B passed]
+# C: [A + B]
 
+
+# Read output, loop for C value
+node $main --a 1 --b 2
+
+# Print in BASH as well
+headline a b
+
+# Take B and C, and funnel it back into node
 
 
 # PID=$!
 # sudo kill -INT $PID
-
-headline a b
-
 
 
 echo "end of script"
