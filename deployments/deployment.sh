c3f41fcf8703290ed0016b6e863d055e5c37feab
Luiserebii <luis@serebii.io>
2019-04-25 22:08:06 -0400

    Added some fun styling to script

--- deployments/deployment.sh
+++ deployments/deployment.sh
@@ -4,5 +4,23 @@
 # Main deployment script; this calls main-deploy.js, and executes these in order. A hacky solution to bring order to chaos
 #
 
-node ./main-deploy.js --stage 1
+headline () {
+  #ANSI Escape Code styling
+  border="\e[1;34m=========================="
+  echo -e $border
+  echo -e "Stage $1 - COMPLETE"
+  echo -e $border
+  # Reset coloring
+  echo -e "\e[0m"
+}
+
+#node ./main-deploy.js --stage 1
+headline 1
+
+
+
+echo "end of script"
+
+##########################
+
 
