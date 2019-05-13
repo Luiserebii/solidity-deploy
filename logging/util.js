edda91bcbb7037b367d59c439939e290fa3d6229
Luiserebii <luis@serebii.io>
2019-05-13 04:01:51 -0400

    LogUtil documented.

--- logging/util.js
+++ logging/util.js
@@ -1,9 +1,19 @@
 
 const inquirer = require('inquirer');
 
+/**
+ * Logging utility class, primarily for tasks that fall outside the scope of Logger, especially in receiving and interacting with user input.
+ */
 class LogUtil {
 
 
+  /**
+   * A convenient function to use with any functions that may not always necessarily receive their arguments (e.g. bash shell calling a node script through stages, if things break and the next function dependent on the previous one has an undefined variable, simply prompt the user for it and keep running).
+   *
+   * If the variable has not been given a value (*more strictly, if falsy*), let the user know, request a new value, and continue, unless told to otherwise. 
+   * 
+   * 
+   */
   async promptExistence(name, x) {
     console.log("Prompting existence...")
     if(!x) {
