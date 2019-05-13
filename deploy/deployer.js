646b1b8ee70a3ad922f2544ab51cc722eb11fe81
Luiserebii <luis@serebii.io>
2019-05-13 03:18:15 -0400

    Finished documenting Deployer class

--- deploy/deployer.js
+++ deploy/deployer.js
@@ -5,8 +5,19 @@ const pp = new PrettyPrint();
 const Logger = require('../logging/logger');
 const ora = require('ora');
 
+/** Deployer class. Handles all functionality with using web3 and compiled material to deploy! */
 class Deployer {
 
+  /**
+   * Instantiate a Deployer object.
+   * 
+   * <b>NOTE: This is not inteded to be called by the user; if you are looking to create a Deployer from web3, please use "static async build" instead!</b>
+   *
+   * @param {web3_instance} _web3 - Initizalized web3 instance, with provider set
+   * @param {object} [_compiled=null] - solc compilation output
+   * @param {string[]} [_accounts=null] - Array of accounts
+   * @param {Logger.state.ENUM} [_logSetting=Logger.state.NORMAL] - Logger state for deployer to use when running methods.
+   */
   //_compiled as an optional param, a way to sort of... hmmm, should be property?
   //NOTE: THIS IS NOT INTENDED TO BE CALLED; ONLY FOR PRIVATE USE. PLEASE USE BUILD INSTEAD!!!
   constructor(_web3, _compiled = null, _accounts = null, _logSetting = Logger.state.NORMAL) {
@@ -16,13 +27,31 @@ class Deployer {
     this.log = new Logger(_logSetting);
   }
 
-  //Not quite our builder pattern; here, we simply provide an alternate constructor
-  //Funny that a Deployer can return more deployers, but ehhh, ok
+  /**
+   * Construct a Deployer object. <b>The intended point of entry.</b>
+   *
+   * @param {web3_instance} _web3 - Initizalized web3 instance, with provider set
+   * @param {JSON} [_compiled=null] - solc compilation output. Required for using `async deploy()` function!
+   * @param {Logger.state.ENUM} [_logSetting=Logger.state.NORMAL] - Logger state f
+or deployer to use when running methods. 
+   *
+   * @return {Deployer} deployer - a new Deployer object instantiated using the provided parameters.
+   */
   static async build(_web3, _compiled = null, _logSetting = Logger.state.NORMAL) {    
     let accounts = await _web3.eth.getAccounts();  
     return new Deployer(_web3, _compiled, accounts, _logSetting);
   }
 
+  /**
+   * Deploy a contract by name, by parsing the object's (if not provided) compiled material. Returns the contract deployed as a web3.eth.Contract object (as provided by web3).
+   * 
+   * @param {string} name - Name of the contract (e.g. Calculator.sol, ERC20.sol)
+   * @param {array} [args=[]] - Constructor arguments for the contract
+   * @param {object} [sender={ from: this.accounts[0] }] - The send object to be passed along in deployment. By default, it is passed an object declaring the 1st account as the sender.
+   * @param {JSON} [compiled=this.compiled] - solc compilation output.
+   *
+   * @return {web3.eth.Contract} - Contract web3 object of the deployed contract.
+   */
   async deploy(name, args = [], sender = { from: this.accounts[0] }, compiled=this.compiled){
     if(!compiled) { throw "No compilation material provided to deployer!"; }
     this.log.print(Logger.state.SUPER, "Accounts:   " + this.accounts + "\n");
@@ -33,6 +62,15 @@ class Deployer {
     return Contract;
   }
 
+  /**
+   * Deploy a contract object [defined internally; see return val of DeployUtil.extractContract].
+   * 
+   * @param {object} contract - Contract object [constructed internally, NOT web3]
+   * @param {array} args - Constructor arguments for the contract
+   * @param {object} sendOptions - The send object to be passed along in deployment. 
+   * 
+   * @return {web3.eth.Contract} - Contract web3 object of the deployed contract.
+   */
   async deployContract(contract, args, sendOptions){
 
     console.log(pp.miniheadline("Deploying " + contract.name + '...'));
