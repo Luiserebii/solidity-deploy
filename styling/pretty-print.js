9462a4fc11b16a77806eaee808d5ce400dfc17df
Luiserebii <luis@serebii.io>
2019-05-13 04:07:32 -0400

    Documented PrettyPrint

--- styling/pretty-print.js
+++ styling/pretty-print.js
@@ -1,17 +1,25 @@
 
-//Print functions (returns a string, which is then printed):
-//
-// for "> "
-// for headlines (adding ==== below as well)
-// for mini-headlines (adding ---- below as well)
-
+/**
+ * A class consisting of print styling functions (returns a string, which is then, presumably passed to a print function):
+ *
+ * for headlines (adding ==== below as well)
+ * for mini-headlines (adding ---- below as well)
+ */
 class PrettyPrint {
-  
+ 
+  /**
+   * Add an "> " prefix to the argument and return.
+   * @param {*} str
+   */ 
   arrow(str) {
     return "> " + str;
   }
   
-
+  /**
+   * Add a headline to the argument and return. The headline is as long as the argument in characters. 
+   * @param {*} str
+   * @param {string} symbol - Symbol to repeat as a headline
+   */
   headline(str, symbol) {
     let output = "";
     output += str;
@@ -19,10 +27,18 @@ class PrettyPrint {
     return output;
   }
 
+  /**
+   * Add a main headline to the argument and return, all consisting of "="s.
+   * @param {*} str
+   */
   mainheadline(str) {
     return this.headline(str, "=");
   }
 
+  /**
+   * Add a main headline to the argument and return, all consisting of "-"s.
+   * @param {*} str
+   */
   miniheadline(str) {
     return this.headline(str, "-");
   }
