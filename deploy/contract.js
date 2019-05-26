2699e092539d3edb09f734adf6f7297a82afa898
Luiserebii <luis@serebii.io>
2019-05-26 03:00:25 -0400

    Added documentation to Contract class with properties

--- deploy/contract.js
+++ deploy/contract.js
@@ -1,5 +1,7 @@
 /**
  * Contract class for providing a simple interface for classes to read a specific contract from compiler output.
+ *
+ * For more information on these properties, please see the Solidity documentation on solc regarding the output: https://solidity.readthedocs.io/en/v0.5.8/using-the-compiler.html#output-description
  */
 class Contract {
 
@@ -15,22 +17,42 @@ class Contract {
     this.solFile = sol;
   }
 
+  /**
+   * Returns the ABI object as defined within the JSON.
+   * @return {JSON} ABI
+   */
   get abi() { 
     return this.raw.abi; 
   }
 
+  /**
+   * Returns the compiled bytecode of the contract, as a hex string.
+   * @return {string} bytecode
+   */
   get bytecode() {
     return this.raw.evm.bytecode.object; 
   }
 
+  /**
+   * Returns the compiler metadata.
+   * @return {JSON} metadata
+   */
   get metadata() { 
     return JSON.parse(this.raw.metadata); //the metadata object is just a string, so let's parse for convenience...
   }   
 
+  /**
+   * Returns the compiler version.
+   * @return {string} version
+   */
   get compilerVersion() { 
     return this.metadata.compiler.version; 
   }
 
+  /**
+   * Returns the optimizer metadata object.
+   * @return {JSON} optimizer
+   */
   get optimizer() {
     return this.metadata.settings.optimizer; 
   }
